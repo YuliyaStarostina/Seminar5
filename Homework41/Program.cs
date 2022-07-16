2da9137fc313dc3d95a8ac3b82831a353c2f6d34
Starostina <pankovays@mail.ru>
2022-07-16 16:48:06 +0700

    Домашнее задание к семинару 6  задача 41

---
+++ Homework41/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+System.Console.WriteLine("Введите числа через запятую");
+string[] text = Console.ReadLine()!.Split(",");
+int count = 0;
+for (int i = 0; i < text.Length; i++)
+{
+ int numbers = int.Parse(text[i]);
+ if (numbers > 0) count ++;
+}
+System.Console.WriteLine($"Количество чисел больше нуля {count}");
